96e29aec73d30931f4e3a9ec5b49b29a0a811de4
justin <justinemsengi@gmail.com>
2023-07-17 09:46:41 +0300

    So challenging xylophone app imposing audios but there is something new to learn

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: (){
+                final player = AudioPlayer();
+                player.play(DeviceFileSource('note1.wav'));
+              }, child: Text('Click Me'),
+            ),
+            ),
+          ),
+        ),
+    );
+  }
+}
